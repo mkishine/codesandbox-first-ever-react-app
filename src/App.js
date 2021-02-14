4981fdd654ff9c10f4f8f627c53f79eb9d9ce4a8
mkishine <mkishine@users.noreply.github.com>
2021-02-14 17:03:17 -0500

    converted jsoneditor example App.js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+// inspired by https://github.com/josdejong/jsoneditor/blob/develop/examples/react_demo/src/App.js
 import React from "react";
-import "./styles.css";
 
-export default function App() {
+import JSONEditorDemo from "./JSONEditorDemo";
+import "./App.css";
+
+function App() {
+  const [json, setJson] = React.useState({
+    array: [1, 2, 3],
+    boolean: true,
+    null: null,
+    number: 123,
+    object: { a: "b", c: "d" },
+    string: "Hello World"
+  });
+
+  const onChangeJSON = (json) => {
+    setJson({ json });
+  };
+
+  const updateTime = () => {
+    const time = new Date().toISOString();
+    setJson({ ...json, time });
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3>Let me try and save it</h3>
+    <div className="app">
+      <h1>JSONEditor React demo</h1>
+      <div className="contents">
+        <div className="menu">
+          <button onClick={updateTime}>Create/update a field "time"</button>
+        </div>
+        <JSONEditorDemo json={json} onChangeJSON={onChangeJSON} />
+        <div className="code">
+          <pre>
+            <code>{JSON.stringify(json, null, 2)}</code>
+          </pre>
+        </div>
+      </div>
     </div>
   );
 }
+export default App;
